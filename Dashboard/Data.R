a17c11942a4b4f9ccf1993a6d85796a691ea5c69
lucasmacenagg <81395149+lucasmacenagg@users.noreply.github.com>
2024-03-10 21:18:56 -0300

    Update Data.R

--- Dashboard/Data.R
+++ Dashboard/Data.R
@@ -1,5 +1,14 @@
 data <- read.csv('koreanTV.csv')
-titulo <- data$Title
-genero <- data$Genre
-ano <- as.numeric(substr(data$Year, start= 2,stop= 5))
-ranking <- data$Rating
+data_limpo <- subset(data, !grepl("-", Rating) & !grepl("-", Year) & !grepl("-", Genre) & !grepl("-", Time) & !grepl("-", Votes.) & !grepl("-", Stars))
+titulo <- data_limpo$Title
+genero <- data_limpo$Genre
+ano <- as.numeric(substr(data_limpo$Year, start = 2, stop = 5))
+ranking <- as.numeric(data_limpo$Rating)
+
+media_ano = mean(ano)
+mediana_ano = median(ano)
+desvio_ano = sd(ano)
+
+media_classificação = mean(ranking)
+mediana_classificação = median(ranking)
+desvio_classificação = sd(ranking)
